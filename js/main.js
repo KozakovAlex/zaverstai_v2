9d475038eba2b529fee9970aa77ba9b027cc9fff
KozakovAlex <72526330+KozakovAlex@users.noreply.github.com>
2021-01-05 23:22:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+//up-button script
 let upButton = document.querySelector('.up-button');
 
 window.onscroll = function () {
@@ -6,8 +8,22 @@ window.onscroll = function () {
   } else {
     upButton.classList.remove('up-button_shown');
   }
-}
+};
 
 upButton.onclick = function () {
   window.scrollTo(0, 0)
 }
+
+//select script
+let cards = document.querySelectorAll('.js-card');
+let articlesSelect = document.querySelector('#articles__select');
+
+articlesSelect.onchange = () => {
+  for (let card of cards) {
+    if (card.dataset.theme !== articlesSelect.value && articlesSelect.value !== 'all') {
+      card.classList.add('js-card_hidden');
+    } else {
+      card.classList.remove('js-card_hidden');
+    }
+  }
+};
